0229694935eee64d6ad2d5085fbeb6ee8cd6f055
jviding <jasu.viding@helsinki.fi>
2016-02-14 05:17:50 +0300

    ready

---
+++ hello.js
@@ -0,0 +1,38 @@
+var request = require('./requests');
+
+timeToUpdate(function (item) {
+  // if update fails, maybe token has expired
+  if (item === false) {
+    timeToAuth(function (item) {
+      // if auth succeeds, try to update again
+      if (item === true) {
+        timeToUpdate();
+      }
+    });
+  }
+  // else everything is well
+});
+
+function timeToUpdate (callback) {
+  request.setIp(function (item) {
+    if (typeof callback === 'function' && callback()) {
+      if (JSON.parse(item).success === false) {
+        callback(false);
+      } else {
+        callback(true);
+      }
+    }
+  });
+};
+
+function timeToAuth (callback) {
+  request.authenticate(function (item) {
+    if (typeof callback === 'function' && callback()) {
+      if (item.success === false) {
+        callback(false);
+      } else {
+        callback(true);
+      }
+    }
+  });
+};
---
+++ requests.js
@@ -0,0 +1,48 @@
+var request = require('request');
+var secret = require('./secret');
+var fs = require('fs');
+var tokenFile = 'token.txt';
+var url = 'http://cloud.mrjasu.com/api';
+
+module.exports = {
+	// Authenticate user
+	authenticate: function authenticate (callback) {
+		request.post({url: url+'/authenticate', 
+		  form: {
+		    name: secret.username,
+		    password: secret.password
+		  }}, function (err, httpResponse, body) {
+		    if (err) throw err;
+		    var res = JSON.parse(httpResponse.body);
+		    writeToken(res.token);
+		    callback(res);
+		});
+	},
+	// Upload current IP
+	setIp: function setIp (callback) {
+		readToken(function (token) {
+			request({
+				url: url+'/setip',
+				headers: {
+					'x-access-token': token
+				}
+			}, function (err, response, body) {
+				if (err) throw err;
+				callback(body);
+  			});
+		});
+	}
+};
+
+function writeToken (token) {
+	fs.writeFile(tokenFile, token, function (err) {
+		if (err) throw err;
+	});
+};
+
+function readToken (callback) {
+	fs.readFile(tokenFile, 'utf8', function (err, data) {
+  		if (err) throw err;
+  		callback(data);
+	});
+};
